57a3167132a5870833cf26930639c7d345a08da2
Rachit-2409 <108273325+Rachit-2409@users.noreply.github.com>
2024-08-09 13:24:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let btn = document.getElementById('Btn');
+let image = document.getElementById('Dice_image');
+let result_box = document.querySelector('ul');
+btn.addEventListener('click', diceroll);
+let number = 1;
+
+function diceroll(){
+    
+   /*Dice Result*/
+    let value =  Math.floor(Math.random()*6);
+    image.src = `./assests/side_${value + 1 }_pips.png`;
+
+
+    /*Li */ 
+    let ele =  document.createElement('li');
+    result_box.appendChild(ele);
+
+    /*result Image*/
+    let image_result =  document.createElement('img');
+    image_result.src = `./assests/side_${value + 1 }_pips.png`;
+
+    let text = document.createElement('p');
+    text.innerText = 'Roll : ' + number; 
+    console.log(number);
+
+    
+    ele.appendChild(text);
+    ele.appendChild(image_result);
+
+    number++;
+
+}
